16b862522b0a115cd6cece348a3eb0f6caf2f81e
Beatriz Feliciano <beatrizfeliciano1999@gmail.com>
2026-05-24 00:34:19 +0100

    adjust deployment pointing

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/feira-do-livro-2026/',
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+  },
 })
